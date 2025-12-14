20171532be038cfac0c39001d69a31c76f7a867b
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-12-13 17:31:00 -0700

    new infra
    * remove old pre-commit
    * enable renovate bot
    * linters changed and fixed
    
    update flake and dockerfile

--- flake.nix
+++ flake.nix
@@ -1,40 +1,95 @@
-# in flake.nix
 {
+  description = "Consumer repo using shared base + rust precommit system";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    precommit.url = "github:FredSystems/pre-commit-checks";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-          libPath = with pkgs; lib.makeLibraryPath [
-            libGL
-            libxkbcommon
-            wayland
-          ];
-          rustToolchain = pkgs.rust-bin.stable.latest.default;
-          # new! 👇
-          nativeBuildInputs = with pkgs; [ rustToolchain ];
-          # also new! 👇
-          buildInputs = with pkgs; [ cargo-make cargo-deny cargo-machete cargo-profiler samply cargo-tauri typos ];
-          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-          LD_LIBRARY_PATH = libPath;
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell {
-            # 👇 and now we can just inherit them
-            inherit buildInputs nativeBuildInputs RUST_SRC_PATH LD_LIBRARY_PATH;
-          };
-        }
-      );
-}
+  outputs =
+    {
+      self,
+      precommit,
+      nixpkgs,
+      ...
+    }:
+    let
+      systems = precommit.lib.supportedSystems;
+    in
+    {
+      ##########################################################################
+      ## CHECKS — unified base+rust via mkCheck
+      ##########################################################################
+      checks = builtins.listToAttrs (
+        map (system: {
+          name = system;
+          value = {
+            pre-commit-check = precommit.lib.mkCheck {
+              src = ./.;
 
-# https://www.reddit.com/r/rust/comments/mmbfnj/nixifying_a_rust_project/
+              inherit system;
+              check_rust = true;
+              check_docker = true;
+
+              extraExcludes = [
+                "^speed_tests/"
+                "^Documents/"
+                "^res/"
+                "typos.toml"
+              ];
+            };
+          };
+        }) systems
+      );
+
+      ##########################################################################
+      ## DEV SHELLS — merged env + your extra Rust goodies
+      ##########################################################################
+      devShells = builtins.listToAttrs (
+        map (system: {
+          name = system;
+
+          value =
+            let
+              pkgs = import nixpkgs { inherit system; };
+
+              # Unified check result (base + rust)
+              chk = self.checks.${system}."pre-commit-check";
+
+              # Packages that git-hooks.nix / mkCheck say we need
+              corePkgs = chk.enabledPackages or [ ];
+
+              # Extra Rust / tooling packages (NO extra rustc here)
+              extraRustTools = [
+              ];
+              # ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+              # ];
+
+              # Extra dev packages provided by mkCheck (includes rustToolchain)
+              extraDev = chk.passthru.devPackages or [ ];
+
+              # Library path packages: whatever mkCheck wants + your GL/Wayland bits
+              libPkgs = [ ];
+              # (chk.passthru.libPath or [ ])
+              # ++ [
+              # ]
+              # ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+              # ];
+            in
+            {
+              default = pkgs.mkShell {
+                buildInputs = extraDev ++ corePkgs ++ extraRustTools;
+
+                LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libPkgs;
+
+                shellHook = ''
+                  ${chk.shellHook}
+
+                  alias pre-commit="pre-commit run --all-files"
+                '';
+              };
+            };
+        }) systems
+      );
+    };
+}
